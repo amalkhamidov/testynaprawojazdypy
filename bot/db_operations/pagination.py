d12f4c8000a62ae9f1c54213733422d07fbdf1f5
amalkhamidov <akoteykula@gmail.com>
2023-03-21 23:54:19 +0100

    feat: ⏩ pagination disable

--- bot/db_operations/pagination.py
+++ bot/db_operations/pagination.py
@@ -1,9 +1,9 @@
-from aiogram import types
+"""from aiogram import types
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def create_pagination_buttons(current_page: int, total_pages: int, page_callback_prefix: str):
-    markup = InlineKeyboardBuilder()
+    keyboard = InlineKeyboardBuilder()
 
     # Define button labels
     first_page = '«1'
@@ -14,18 +14,25 @@ def create_pagination_buttons(current_page: int, total_pages: int, page_callback
 
     # Add buttons based on the current page
     if current_page > 1:
-        markup.add(
+        keyboard.add(
             types.InlineKeyboardButton(text=first_page, callback_data=f'{page_callback_prefix}:1'),
             types.InlineKeyboardButton(text=previous_page, callback_data=f'{page_callback_prefix}:{current_page - 1}')
         )
 
-    markup.add(types.InlineKeyboardButton(text=current_page_label, callback_data='current_page'))
+    keyboard.add(types.InlineKeyboardButton(text=current_page_label, callback_data='current_page'))
 
     if current_page < total_pages:
-        markup.add(
+        keyboard.add(
             types.InlineKeyboardButton(text=next_page, callback_data=f'{page_callback_prefix}:{current_page + 1}'),
             types.InlineKeyboardButton(text=last_page, callback_data=f'{page_callback_prefix}:{total_pages}')
         )
 
-    return markup
+    return keyboard
 
+
+print(
+    [i.text for i in create_pagination_buttons(2, 86, 'page').buttons]
+)
+"""
+# Deprecated, a lot of bugs
+# Path: bot/db_operations/pagination.py
